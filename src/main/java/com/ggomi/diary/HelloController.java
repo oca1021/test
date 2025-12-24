c1e96da1459a14d0d100e16eb3abfdbed671ac48
test <test>
2025-12-24 22:27:36 +0900

    Create new

---
+++ src/main/java/com/ggomi/diary/HelloController.java
@@ -0,0 +1,13 @@
+package com.ggomi.diary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    //
+    @GetMapping("hello")
+    public String hello() {
+        return "안녕하세용";
+    }
+}
